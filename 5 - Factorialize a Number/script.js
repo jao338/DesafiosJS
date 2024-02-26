174cf8e0fb28a23f0bdf74ce530d47c17bc55e26
João Henrique <joaohenriquerc123@gmail.com>
2024-02-26 17:20:39 -0300

    Ajuste

---
+++ 5 - Factorialize a Number/script.js
@@ -0,0 +1,13 @@
+let input = document.querySelector("#inputAsk");
+
+input.addEventListener("input", function(){
+    
+    /*
+    for (let index = input.value.length; index > input.value.length; index--) {
+        console.log(index);
+    }
+
+    */
+
+    console.log(input.value.length);
+});
